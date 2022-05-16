6e4f6992bb4cc26c72925814b1297a7a1a3be7c6
MousaHussen <Mousahusseen@gmail.com>
2022-05-16 19:18:41 +0300

    connected cloudenetry with react

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
-  image :{type: String},
+  image: { type: String },
   lastName: { type: String },
   age: { type: Number, required: true },
   country: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  //likes:[{posts}]
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
 });
 
 userSchema.pre("save", async function () {
   this.email = this.email.toLowerCase();
   this.password = await bcrypt.hash(this.password, 10);
-}); 
+});
 
 module.exports = mongoose.model("User", userSchema);
